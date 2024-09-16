02219829478a7e2dd56ce58d54d8068e47fa0bc7
pokitong <yujusul@gmail.com>
2024-09-16 21:52:04 +0900

    섹션3 챕터5 Node.js 라이브러리 사용하기 강의 수강 완료

--- section03/src/index.js
+++ section03/src/index.js
@@ -3,8 +3,13 @@ import mul, {add, sub} from "./math.js";
 // default 로 export 한 모듈의 경우 {} 을 사용하지 않고 이름을 그대로 사용해서 임포트 할 수 있다.
 // 또한 해당 함수의 이름 그대로가 아닌 다른 이름으로 불러와도 사용 가능하다.
 
+import randomColor from "randomcolor";
+// 라이브러리에서 import 시에는 확장자명 없이 해당 라이브러리 이름만 명시하면 된다.
+
+const color = randomColor();
+console.log(color);
 
 // ESmodule 시스템을 이용할 때는 임포트 시 파일의 확장자까지 명시해 주어야 한다.
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
