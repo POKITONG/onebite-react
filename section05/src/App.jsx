7c1e20e25d09a59007420f728f19389750e69b28
pokitong <yujusul@gmail.com>
2024-09-17 00:52:45 +0900

    섹션5 챕터7 State와 Props 강의 수강 완료

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,31 +1,26 @@
 import './App.css'
 import {useState} from "react";
+import Bulb from "./components/Bulb.jsx";
+import Counter from "./components/Counter.jsx";
 
 // vite 로 만든 react 프로젝트에서는 from 뒤의 확장자를 적지 않아도 되도록 내부적으로 자동 설정되어 있다.
+
+
 function App() {
-    const [count, setCount] = useState(0);
-    const [light, setLight] = useState("OFF");
 
     return (
         <>
-            <div>
-                <h1>{light}</h1>
-                <button onClick={() => {
-                    setLight(light === "ON" ? "OFF" : "ON");
-                }}> 전구 {light === "ON"? "끄기" : "켜기"}
-                </button>
-            </div>
-            <div>
-                <h1>{count}</h1>
-                <button onClick={() => {
-                    setCount(count + 1);
-                }}>+
-                </button>
-            </div>
+            <Bulb/>
+            <Counter/>
         </>
     );
 }
 
 // html 을 반환하는 자바스크립트 파일 : 컴포넌트
+
+// 리액트에서 컴포넌트가 리렌더링 될 때
+// 1. 자신이 관리하는 State 의 상태가 변경되었을 때
+// 2. 자신이 재공받는 props 의 값이 변경되었을 때
+// 3. 부모 컴포넌트가 리렌더링 될 때
 
 export default App
