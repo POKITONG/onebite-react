dd978ba79e0c0acb6fd6cf5459f7d01ba468e75a
pokitong <yujusul@gmail.com>
2024-09-17 00:40:57 +0900

    섹션5 챕터6 State로 상태관리하기 강의 수강 완료

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,30 +1,27 @@
 import './App.css'
-import Header from "./components/Header";
+import {useState} from "react";
+
 // vite 로 만든 react 프로젝트에서는 from 뒤의 확장자를 적지 않아도 되도록 내부적으로 자동 설정되어 있다.
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-import Button from "./components/Button"
-
 function App() {
+    const [count, setCount] = useState(0);
+    const [light, setLight] = useState("OFF");
 
-    const buttonProps = {
-        text: "메일",
-        color: "red",
-        a: 1,
-        b: 2,
-        c: 3,
-
-    }
     return (
         <>
-            {/*<Header/>*/}
-            {/*<Main/>*/}
-            {/*<Footer/>*/}
-            <Button {...buttonProps}/>
-            <Button text={"카페"}/>
-            <Button text={"블로그"}>
-                <Header/>
-            </Button>
+            <div>
+                <h1>{light}</h1>
+                <button onClick={() => {
+                    setLight(light === "ON" ? "OFF" : "ON");
+                }}> 전구 {light === "ON"? "끄기" : "켜기"}
+                </button>
+            </div>
+            <div>
+                <h1>{count}</h1>
+                <button onClick={() => {
+                    setCount(count + 1);
+                }}>+
+                </button>
+            </div>
         </>
     );
 }
