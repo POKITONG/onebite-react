8ec30573bb6c1d3e56e7c19090a7a7cec6e3733d
pokitong <yujusul@gmail.com>
2024-09-18 17:09:05 +0900

    섹션8 챕터3 기능 구현 준비하기 강의 수강 완료

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -4,15 +4,39 @@ import Header from "./components/Header.jsx";
 import Editor from "./components/Editor.jsx";
 import List from "./components/List.jsx";
 
+const mockData = [
+    {
+        id: 0,
+        isDone: false,
+        content: "React 공부하기",
+        date: new Date().getTime(),
+    },
+    {
+        id: 1,
+        isDone: false,
+        content: "빨래하기",
+        date: new Date().getTime(),
+    },
+    {
+        id: 2,
+        isDone: false,
+        content: "노래 연습하기",
+        date: new Date().getTime(),
+    }
+];
+
 function App() {
 
-  return (
-    <div className="App">
-        <Header/>
-        <Editor/>
-        <List/>
-    </div>
-  )
+    const [todos, setTodos] = useState(mockData);
+
+
+    return (
+        <div className="App">
+            <Header/>
+            <Editor/>
+            <List/>
+        </div>
+    )
 }
 
 export default App
