d554185779186d3eb5cd9a3f4f8af3fd5fae4b41
pokitong <yujusul@gmail.com>
2024-09-19 00:08:02 +0900

    섹션12 챕터4 페이지 라우팅 3.페이지 이동 강의 수강 완료

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,6 +1,6 @@
 import {useState} from 'react'
 import './App.css'
-import {Route, Routes} from "react-router-dom";
+import {Link, Route, Routes, useNavigate} from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import New from "./pages/New.jsx";
 import Diary from "./pages/Diary.jsx";
@@ -11,18 +11,36 @@ import Notfound from "./pages/Notfound.jsx";
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 
 function App() {
+    const nav = useNavigate();
+    // useNavigate : 페이지를 실제로 이동시키는 네비게이트 함수를 반환해 줍니다.
 
+    const onClickButton = () => {
+        nav("/new");
+    };
+
+    // a 태그와 같이 링크가 필요한 경우 : Link 컴포넌트 사용
+    // 이벤트 핸들러 함수 내에서 특정 조건에 따라서 페이지를 이동시켜야 하는 경우 : useNavigate 훅 사용
 
     return (
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/new" element={<New/>}/>
-            <Route path="/diary" element={<Diary/>}/>
-            <Route path="*" element={<Notfound/>}/>
-        </Routes>
+        <>
+            <div>
+                <Link to={"/"}>Home</Link>
+                <Link to={"/new"}>New</Link>
+                <Link to={"/diary"}>Diary</Link>
+                {/* Link : html 의 a 태그를 대체하는 컴포넌트
+                페이지 이동시 a 태그와 다르게 새로고침이 되지 않는다.*/}
+            </div>
+            <button onClick={onClickButton}>New 페이지로 이동</button>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/new" element={<New/>}/>
+                <Route path="/diary" element={<Diary/>}/>
+                <Route path="*" element={<Notfound/>}/>
+            </Routes>
+        </>
         // 1. Routes 컴포넌트 내부에는 Route 컴포넌트만 들어갈 수 있다.
         // 2. Routes 컴포넌트 외부 요소는 모든 페이지에 전부 렌더링 된다. (Header, Footer)
-    )
+    );
 }
 
 export default App
