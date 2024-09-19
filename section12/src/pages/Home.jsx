0854d5785e4389f18eea8a0337b3a38e4cf9f830
pokitong <yujusul@gmail.com>
2024-09-19 09:52:01 +0900

    섹션12 챕터10 Home 페이지 구현하기 1.UI 강의 수강 완료

--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -1,10 +1,19 @@
 import {useSearchParams} from "react-router-dom";
+import Header from "../components/Header.jsx";
+import Button from "../components/Button.jsx";
+import DiaryList from "../components/DiaryList.jsx";
 
 const Home = () => {
     // const [params, setParams] = useSearchParams();
     // console.log(params.get("value"));
 
-    return <div>Home</div>
+    return <div>
+        <Header title={"2024년 2월"}
+        leftChild={<Button text={"<"}/>}
+        rightChild={<Button text={">"}/>}
+        />
+        <DiaryList/>
+    </div>
 }
 
 export default Home;
